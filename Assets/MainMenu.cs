edeabc6309dc349f62095138436a52e1e0fa3aa7
Justin Lonh <justind6n423@gmail.com>
2019-02-16 16:27:35 -0800

    added start menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        FindObjectOfType<AudioManager>().Play("BalloonPop");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
