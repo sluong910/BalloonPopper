70fd55df36889fcc88a0a4eadd873689e29b6e92
Joseph Yeung <yeungjoseph10@gmail.com>
2019-02-17 11:04:39 -0800

    donzos'

--- Assets/Scripts/GameFinsihed.cs
+++ Assets/Scripts/GameFinsihed.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameFinsihed : MonoBehaviour
 {
     Animator anim;
+    private bool setText = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,30 @@ public class GameFinsihed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-        if (Input.GetMouseButtonDown(0) && stateInfo.normalizedTime >= 1)
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);        
+        if (stateInfo.normalizedTime >= 1)
         {
-            SceneManager.LoadScene("Menu");
+            // Show label           
+            if (!setText)
+            {
+                GameObject go = new GameObject("Child");
+                go.AddComponent<RectTransform>();
+                go.transform.SetParent(this.transform);                
+                go.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 100);
+                go.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 500);
+                go.transform.localPosition = new Vector3(0, -250f, 0);                
+
+                go.AddComponent<Text>().text = "TAP TO RETURN TO IRVINE";
+                go.GetComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+                go.GetComponent<Text>().fontStyle = FontStyle.Bold;
+                go.GetComponent<Text>().color = Color.white;
+                go.GetComponent<Text>().fontSize = 35;
+
+                setText = true;
+            }            
+
+            if (Input.GetMouseButtonDown(0))            
+                SceneManager.LoadScene("Menu");
         }
     }
 }
